e119b69cca76bbe3c197561d52d0a901fad823b1
Mykola Vasyl'yev <mykovasyl@gmail.com>
2023-02-08 14:25:39 -0500

    set up password for users, create routes and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def index
+    all_users
+    render json: all_users
+  end
+
+  def create
+    user = User.create!(new_user_params)
+    session[:user_id] = user.id
+    render json: user, status: :accepted
+  end
+
+  def show
+    render json: @current_user
+  end
+
+  def update
+    user_to_update = find_user
+    user_to_update.update!(update_user_params)
+    render json: user_to_update, status: :accepted
+  end
+
+  def destroy
+    user_to_delete = find_user
+    user_to_delete.destroy
+    head :no_content
+  end
+
+  private
+
+  def all_users
+    User.all
+  end
+
+  def new_user_params
+    params.permit(:username, :password, :password_confirmation, :name)
+  end
+
+  def update_user_params
+    params.permit(:name)
+  end
+
+  def find_user
+    User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
+  resources :users
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
