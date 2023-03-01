9f7abab1c386c230eb735086f1bde050fb2f646e
Mykola Vasyl'yev <mykovasyl@gmail.com>
2023-03-01 17:37:49 -0500

    set up drawer with errors to be resolved

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,8 +7,31 @@ import Home from "./Home";
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
 import { Route, Routes, useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { AppBar } from "@mui/material";
 
 export const UserContext = createContext();
+
+// export function PersistentDrawerLeft() {
+//   const theme = useTheme();
+//   const [open, setOpen] = React.useState(false);
+
+//   const handleDrawerOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleDrawerClose = () => {
+//     setOpen(false);
+//   };
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
@@ -16,6 +39,55 @@ function App() {
   const [errors, setErrors] = useState([]);
 
   const navigate = useNavigate();
+
+  const drawerWidth = 240;
+
+  const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    })
+  );
+
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== "open",
+  })(({ theme, open }) => ({
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
+
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  }));
+
+  // end of drawer code
 
   // fetch current user else no user logged in
   useEffect(() => {
@@ -66,36 +138,111 @@ function App() {
   }
 
   return (
-    <div>
-      <UserContext.Provider
-        value={{
-          currentUser,
-          setCurrentUser,
-          avatar,
-          handleLogOut,
-          handleAddRecipe,
-          handleDeleteRecipe,
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position='fixed' open={open}>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap component='div'>
+            booka
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
         }}
+        variant='persistent'
+        anchor='left'
+        open={open}
       >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
         <NavBar />
-        <Routes>
-          <Route path='/randomrecipe' element={<RandomRecipe />} />
-          <Route path='/recipeform' element={<RecipeForm />} />
-          <Route path='/recipebook' element={<RecipeBook />} />
-          <Route path='/login' element={<LogIn />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route exact path='/' element={<Home />} />
-          <Route
-            path='*'
-            element={
-              <>
-                <h1>404 not found</h1>
-              </>
-            }
-          />
-        </Routes>
-      </UserContext.Provider>
-    </div>
+        <Divider />
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <UserContext.Provider
+          value={{
+            currentUser,
+            setCurrentUser,
+            avatar,
+            handleLogOut,
+            handleAddRecipe,
+            handleDeleteRecipe,
+          }}
+        >
+          <Routes>
+            <Route path='/randomrecipe' element={<RandomRecipe />} />
+            <Route path='/recipeform' element={<RecipeForm />} />
+            <Route path='/recipebook' element={<RecipeBook />} />
+            <Route path='/login' element={<LogIn />} />
+            <Route path='/signup' element={<SignUp />} />
+            <Route exact path='/' element={<Home />} />
+            <Route
+              path='*'
+              element={
+                <>
+                  <h1>404 not found</h1>
+                </>
+              }
+            />
+          </Routes>
+        </UserContext.Provider>
+      </Main>
+    </Box>
+    // <div>
+    // <UserContext.Provider
+    //   value={{
+    //     currentUser,
+    //     setCurrentUser,
+    //     avatar,
+    //     handleLogOut,
+    //     handleAddRecipe,
+    //     handleDeleteRecipe,
+    //   }}
+    // >
+    //     <NavBar />
+    //     <Routes>
+    //       <Route path='/randomrecipe' element={<RandomRecipe />} />
+    //       <Route path='/recipeform' element={<RecipeForm />} />
+    //       <Route path='/recipebook' element={<RecipeBook />} />
+    //       <Route path='/login' element={<LogIn />} />
+    //       <Route path='/signup' element={<SignUp />} />
+    //       <Route exact path='/' element={<Home />} />
+    //       <Route
+    //         path='*'
+    //         element={
+    //           <>
+    //             <h1>404 not found</h1>
+    //           </>
+    //         }
+    //       />
+    //     </Routes>
+    //   </UserContext.Provider>
+    // </div>
   );
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,28 +2,79 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./App";
 import Button from "@mui/material/Button";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 
 function NavBar() {
   const { currentUser, handleLogOut } = useContext(UserContext);
   return (
-    <nav id='navigation'>
+    <List>
       <Link to='/'>Home</Link>
       {currentUser.id ? (
         <>
-          <Link to='/randomrecipe'>Random Recipe</Link>
-          <Link to='/recipebook'>Your Recipe Book</Link>
-          <Link to='/recipeform'>Add Recipe</Link>
-          <Button variant='contained' onClick={handleLogOut}>
-            Log out
-          </Button>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText>
+                <Link to='/'>Home</Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText>
+                <Link to='/randomrecipe'>Random Recipe</Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText>
+                <Link to='/recipebook'>Your Recipe Book</Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText>
+                <Link to='/recipeform'>Add Recipe</Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText>
+                <Button variant='contained' onClick={handleLogOut}>
+                  Log out
+                </Button>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
         </>
       ) : (
         <>
-          <Link to='/login'>Log In</Link>
-          <Link to='/signup'>Sign Up</Link>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText>
+                <Link to='/login'>Log In</Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText>
+                <Link to='/signup'>Sign Up</Link>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
         </>
       )}
-    </nav>
+    </List>
   );
 }
 
