c21b17e946808c00bd090b92e2837a48ce489cbb
Mykola Vasyl'yev <mykovasyl@gmail.com>
2023-03-29 09:22:40 -0400

    style sign up and remove avatar code

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -2,12 +2,24 @@ import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "./App";
 import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function LogIn() {
   const [login, setLogin] = useState({
     username: "",
     password: "",
   });
+  const theme = createTheme();
   const { setCurrentUser } = useContext(UserContext);
   const navigate = useNavigate();
 
@@ -34,39 +46,60 @@ function LogIn() {
   }
 
   return (
-    <>
-      <h2
-        style={{
-          border: ".5px solid grey",
-          marginBottom: "24px",
-          borderRadius: "8px",
-          padding: "8px",
-        }}
-      >
-        Log in:
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <p>Username:</p>
-        <input
-          type='text'
-          name='username'
-          value={login.username}
-          onChange={handleInputChange}
-        />
-
-        <p>Password:</p>
-        <input
-          type='password'
-          name='password'
-          value={login.password}
-          onChange={handleInputChange}
-        />
-
-        <Button type='submit' variant='contained' style={{ margin: "16px" }}>
-          Log in!
-        </Button>
-      </form>
-    </>
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component='h1' variant='h5'>
+            Sign in
+          </Typography>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              label='Username'
+              name='username'
+              autoComplete='username'
+              value={login.username}
+              onChange={handleInputChange}
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              value={login.password}
+              onChange={handleInputChange}
+              autoComplete='current-password'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
