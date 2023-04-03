d949242ba485ecb870c65b396c32d6831fc9b47c
Mykola Vasyl'yev <mykovasyl@gmail.com>
2023-04-03 12:32:50 -0400

    fix deployement

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,6 +8,6 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rake db:migrate
