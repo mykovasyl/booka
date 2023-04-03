7c2ef9f6dcfd35ba0a40b0c0188b522416619f31
Mykola Vasyl'yev <mykovasyl@gmail.com>
2023-04-03 12:44:55 -0400

    changes for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,10 @@
 # exit on error
 set -o errexit
 
-# builds the front end code
+# Add build commands for front end
 rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate 
